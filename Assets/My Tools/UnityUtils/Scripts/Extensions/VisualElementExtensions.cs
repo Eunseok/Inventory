6befa9f83c0ff5cc3814bda6d798fed773b437a4
Eunseok <79277772+Eunseok@users.noreply.github.com>
2025-03-26 05:36:34 +0900

    feat: 인벤토리 아이템 이동

--- Assets/My Tools/UnityUtils/Scripts/Extensions/VisualElementExtensions.cs
+++ Assets/My Tools/UnityUtils/Scripts/Extensions/VisualElementExtensions.cs
@@ -1,46 +1,51 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 
-namespace UnityUtils {
-    public static class VisualElementExtensions {
+namespace UnityUtils
+{
+    public static class VisualElementExtensions
+    {
         /// <summary>
-        /// Creates a new child VisualElement and adds it to the parent.
+        /// 새로운 자식 VisualElement를 생성하고 이를 부모에 추가합니다.
         /// </summary>
-        /// <param name="parent">The parent VisualElement to add the child to.</param>
-        /// <param name="classes">The CSS classes to add to the child.</param>
-        /// <returns>The created child VisualElement.</returns>
-        public static VisualElement CreateChild(this VisualElement parent, params string[] classes) {
+        /// <param name="parent">자식을 추가할 부모 VisualElement입니다.</param>
+        /// <param name="classes">자식 요소에 추가할 CSS 클래스들입니다.</param>
+        /// <returns>생성된 자식 VisualElement를 반환합니다.</returns>
+        public static VisualElement CreateChild(this VisualElement parent, params string[] classes)
+        {
             var child = new VisualElement();
             child.AddClass(classes).AddTo(parent);
             return child;
         }
 
         /// <summary>
-        /// Creates a new child of type T and adds it to the parent.
+        /// 새로 생성된 T 타입의 자식을 부모에 추가합니다.
         /// </summary>
-        /// <typeparam name="T">The type of the child VisualElement.</typeparam>
-        /// <param name="parent">The parent VisualElement to add the child to.</param>
-        /// <param name="classes">The CSS classes to add to the child.</param>
-        /// <returns>The created child VisualElement of type T.</returns>
+        /// <typeparam name="T">자식 VisualElement의 타입입니다.</typeparam>
+        /// <param name="parent">자식을 추가할 부모 VisualElement입니다.</param>
+        /// <param name="classes">자식에게 추가할 CSS 클래스들입니다.</param>
+        /// <returns>생성된 T 타입의 자식 VisualElement입니다.</returns>
         public static T CreateChild<T>(this VisualElement parent, params string[] classes)
-            where T : VisualElement, new() {
+            where T : VisualElement, new()
+        {
             var child = new T();
             child.AddClass(classes).AddTo(parent);
             return child;
         }
 
         /// <summary>
-        /// Adds the child VisualElement to the parent and returns the child.
+        /// 자식 VisualElement를 부모에 추가하고 자식을 반환합니다.
         /// </summary>
-        /// <typeparam name="T">The type of the child VisualElement.</typeparam>
-        /// <param name="child">The child VisualElement to add.</param>
-        /// <param name="parent">The parent VisualElement to add the child to.</param>
-        /// <returns>The added child VisualElement.</returns>
-        public static T AddTo<T>(this T child, VisualElement parent) where T : VisualElement {
+        /// <typeparam name="T">자식 VisualElement의 유형입니다.</typeparam>
+        /// <param name="child">추가할 자식 VisualElement입니다.</param>
+        /// <param name="parent">자식을 추가할 부모 VisualElement입니다.</param>
+        /// <returns>추가된 자식 VisualElement를 반환합니다.</returns>
+        public static T AddTo<T>(this T child, VisualElement parent) where T : VisualElement
+        {
             parent.Add(child);
             return child;
         }
-        
+
         /// <remarks>
         /// See <see cref="AddTo{T}(T, VisualElement)"/> for adding a child to a parent.
         /// </remarks>
@@ -48,52 +53,61 @@ namespace UnityUtils {
             where T : VisualElement => parent.Remove(child);
 
         /// <summary>
-        /// Adds the specified CSS classes to the VisualElement.
+        /// 지정된 ㅕSS 클래스를 VisualElement에 추가합니다.
         /// </summary>
-        /// <typeparam name="T">The type of the VisualElement.</typeparam>
-        /// <param name="visualElement">The VisualElement to add the classes to.</param>
-        /// <param name="classes">The CSS classes to add.</param>
-        /// <returns>The VisualElement with the added classes.</returns>
-        public static T AddClass<T>(this T visualElement, params string[] classes) where T : VisualElement {
-            foreach (string cls in classes) {
-                if (!string.IsNullOrEmpty(cls)) {
+        /// <typeparam name="T">VisualElement의 유형입니다.</typeparam>
+        /// <param name="visualElement">CSS 클래스를 추가할 VisualElement입니다.</param>
+        /// <param name="classes">추가할 CSS 클래스들입니다.</param>
+        /// <returns>CSS 클래스가 추가된 VisualElement를 반환합니다.</returns>
+        public static T AddClass<T>(this T visualElement, params string[] classes) where T : VisualElement
+        {
+            foreach (string cls in classes)
+            {
+                if (!string.IsNullOrEmpty(cls))
+                {
                     visualElement.AddToClassList(cls);
                 }
             }
             return visualElement;
         }
-        
+
         /// <remarks>
-        /// See <see cref="AddClass{T}(T, string[])"/> for adding classes.
+        /// 클래스 추가에 대해선 <see cref="AddClass{T}(T, string[])"/>를 참조하세요.
         /// </remarks>
-        public static void RemoveClass<T>(this T visualElement, params string[] classes) where T : VisualElement {
-            foreach (string cls in classes) {
-                if (!string.IsNullOrEmpty(cls)) {
+        public static void RemoveClass<T>(this T visualElement, params string[] classes) where T : VisualElement
+        {
+            foreach (string cls in classes)
+            {
+                if (!string.IsNullOrEmpty(cls))
+                {
                     visualElement.RemoveFromClassList(cls);
                 }
             }
         }
 
         /// <summary>
-        /// Adds a manipulator to the VisualElement.
+        /// VisualElement에 조작기를 추가합니다.
         /// </summary>
-        /// <typeparam name="T">The type of the VisualElement.</typeparam>
-        /// <param name="visualElement">The VisualElement to add the manipulator to.</param>
-        /// <param name="manipulator">The manipulator to add.</param>
-        /// <returns>The VisualElement with the added manipulator.</returns>
-        public static T WithManipulator<T>(this T visualElement, IManipulator manipulator) where T : VisualElement {
+        /// <typeparam name="T">VisualElement의 유형입니다.</typeparam>
+        /// <param name="visualElement">조작기를 추가할 VisualElement입니다.</param>
+        /// <param name="manipulator">추가할 조작기입니다.</param>
+        /// <returns>조작기가 추가된 VisualElement를 반환합니다.</returns>
+        public static T WithManipulator<T>(this T visualElement, IManipulator manipulator) where T : VisualElement
+        {
             visualElement.AddManipulator(manipulator);
             return visualElement;
         }
-        
+
         /// <summary>
-        /// Sets the background image of a VisualElement using a given Sprite.
+        /// 지정된 Sprite를 사용하여 VisualElement의 배경 이미지를 설정합니다.
         /// </summary>
-        /// <param name="imageContainer">The VisualElement whose background image will be set.</param>
-        /// <param name="sprite">The Sprite to use as the background image.</param>
-        public static void SetImageFromSprite(this VisualElement imageContainer, Sprite sprite) {
+        /// <param name="imageContainer">배경 이미지가 설정될 VisualElement입니다.</param>
+        /// <param name="sprite">배경 이미지로 사용할 Sprite입니다.</param>
+        public static void SetImageFromSprite(this VisualElement imageContainer, Sprite sprite)
+        {
             var texture = sprite.texture;
-            if (texture) {
+            if (texture)
+            {
                 imageContainer.style.backgroundImage = new StyleBackground(texture);
             }
         }
---
+++ Assets/_Project/Scripts/UI/Inventory/Inventory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Systems.Persistence;
+using UnityEngine;
+
+namespace Systems.Inventory {
+    public class Inventory : MonoBehaviour, IBind<InventoryData> {
+        [SerializeField] InventoryView view;
+        [SerializeField] int capacity = 20;
+        [SerializeField] List<ItemDetails> startingItems = new();
+        [field: SerializeField] public SerializableGuid Id { get; set; } = SerializableGuid.NewGuid();
+        
+
+        InventoryController controller;
+        
+        void Awake() {
+            controller = new InventoryController.Builder(view)
+                .WithStartingItems(startingItems)
+                .WithCapacity(capacity)
+                .Build();
+        }
+         
+        public void Bind(InventoryData data) {
+            Debug.Log($"Binding inventory {Id} to data {data.Id}");
+            controller.Bind(data);
+            data.Id = Id;
+        }
+    }
+}
